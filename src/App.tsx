9d3ee10aa2f8fffddeb920c89e264bc2f8af7839
Markevo5150 <marcoAlopez2@outlook.com>
2023-10-26 16:29:15 -0600

    Start

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="upGreen">
+          <span className="title">SPLI</span>
+          <span className="title">TTER</span>
+        </div>
+        <div className="downWhite">
+          <h3 className="section">Bill</h3>
+          <input type="text" placeholder=""></input>
+          <h3 className="section">Select Tip %</h3>
+          <h3 className="section">Number of People</h3>
+          <input type="text" name="" id="" />
+          <div className="summary"></div>
+        </div>
+      </div>
+    </>
+  );
+}
