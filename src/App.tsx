5b8bb7a3ac54bc77dc6ee20033df645d476d76c2
Markevo5150 <marcoAlopez2@outlook.com>
2023-10-27 18:24:30 -0600

    New changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ export default function App() {
     <>
       <div className="main-container">
         <header className="header">
-          <h1 className="header__title">
-            SPLI <br /> TTER
+          <h1 className="calculator__title">
+            SPLI <br />
+            TTER
           </h1>
         </header>
-        <section className="calculator-container">
+        <div className="calculator-container">
           <div className="inputs-section">
             <label htmlFor="bill" className="inputs-section__bill-label">
               Bill
@@ -19,15 +20,19 @@ export default function App() {
               value="0"
             />
             <fieldset className="btns">
-              <legend className="btns__legend">Select Tip %</legend>
+              <legend className="btns__legend">Select Tip%</legend>
               <button className="btns__button">5%</button>
               <button className="btns__button">10%</button>
               <button className="btns__button btns__button--selected">
                 15%
               </button>
-              <button className="btns__button">20%</button>
               <button className="btns__button">25%</button>
-              <input type="number" placeholder="Custom" />
+              <button className="btns__button">50%</button>
+              <input
+                type="number"
+                className="btns__input"
+                placeholder="custom"
+              />
             </fieldset>
             <label htmlFor="people" className="inputs-section__people-label">
               Number of people
@@ -39,29 +44,8 @@ export default function App() {
               value="5"
             />
           </div>
-          <div className="results-section">
-            <div className="results">
-              <div>
-                <p className="results__tip-amount">Tip Amount</p>
-                <p className="results__tip-person">/ person</p>
-              </div>
-
-              <div>
-                <p className="results__tip-value"></p>
-              </div>
-
-              <div>
-                <p className="results__total--amount">Total</p>
-                <p className="results__total--person">/ person</p>
-              </div>
-
-              <div>
-                <p className="results__total--value"></p>
-              </div>
-            </div>
-            <button>RESET</button>
-          </div>
-        </section>
+          div.results-section
+        </div>
         <div class="attribution">
           Challenge by{" "}
           <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
